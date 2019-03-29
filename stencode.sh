32675f630cb4d0396f7552badd1fef80a13f5b0b
Evan Shipman <eshipman@umw.edu>
2019-03-29 11:15:34 -0400

    Added 8-bit decoding

--- stencode.sh
+++ stencode.sh
@@ -30,9 +30,12 @@
 S_WORDS="./small-wordlist.txt"
 L_WORDS="./large-wordlist.txt"
 
+# Encode by default but don't choose a default encoding scheme
+ENCODE=true
 BYTE_ENCODE=false
 WORD_ENCODE=false
 
+# The default output separator is a space
 SEP=" "
 
 # Parse the command line flags
@@ -55,6 +58,10 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
+        -d|--decode)
+            ENCODE=false
+            shift
+            ;;
         *)
             POSITIONAL+=("$1")
             shift
@@ -63,29 +70,59 @@ while [[ $# -gt 0 ]]; do
 done
 set -- "%{POSITIONAL[@]}"
 
-# Operate only on each individual byte
-if [[ ${BYTE_ENCODE} = true ]]; then
-    instream=""
-    input=""
-    # Read the input stream printing out each char/byte converted
-    while read instream; do
-        for i in $(seq 1 ${#instream}); do
-            # Get the decimal number of the character/byte
-            num="$(printf "%d" "'${instream:i-1:1}")"
+# Encode the input
+if [[ ${ENCODE} == true ]]; then
 
-            # Print that line of the file
-            printf "%s" "$(head -${num} ${S_WORDS} | tail -1)"
+    # Operate only on each individual byte
+    if [[ ${BYTE_ENCODE} = true ]]; then
+        instream=""
+        input=""
+        # Read the input stream printing out each char/byte converted
+        while read instream; do
+            for i in $(seq 1 ${#instream}); do
+                # Get the decimal number of the character/byte
+                num="$(printf "%d" "'${instream:i-1:1}")"
 
-            # Add the specified separator
-            if [[ "${i}" -lt "${#instream}" ]]; then
-                printf "%s" "${SEP}"
-            fi
+                # Print that line of the file
+                printf "%s" "$(head -${num} ${S_WORDS} | tail -1)"
+
+                # Add the specified separator
+                if [[ "${i}" -lt "${#instream}" ]]; then
+                    printf "%s" "${SEP}"
+                fi
+            done
         done
-    done
-    printf "\n"
-# Operate on each 16-bit word
-elif [[ ${WORD_ENCODE} = true ]]; then
-    echo "16-bit encoding not implemented yet"
+        printf "\n"
+    # Operate on each 16-bit word
+    elif [[ ${WORD_ENCODE} = true ]]; then
+        echo "16-bit encoding not implemented yet"
+    else
+        echo "Error: No valid encoding option given"
+    fi
+# Decode the input
 else
-    echo "Error: No valid encoding option given"
+    # Decode each word as 1 byte
+    if [[ ${BYTE_ENCODE} = true ]]; then
+        instream=""
+        input=""
+        # Read each input line
+        while read instream; do
+            # Separate each line into an array of words and loop through it
+            arr=( $instream )
+            for i in $(seq 0 $((${#arr[@]} - 1))); do
+                # Get the index of the word from the file
+                index="$(grep -En "^${arr[${i}]}$" "${S_WORDS}" | cut -d ':' -f1)"
+
+                # Alert the user if a word couldn't be found in the word list
+                if [[ "${index}" == "" ]]; then
+                    printf "Error: could not decode word '%s'\n" "${arr[${i}]}"
+                # Otherwise print the ASCII character
+                else
+                    printf "\x$(printf %x ${index})"
+                fi
+            done
+        done
+    elif [[ ${WORD_ENCODE} = true ]]; then
+        echo "16-bit decoding not implemented yet"
+    fi
 fi
